76ffbaa57842add198c59753a09946a18d579abf
Niki Wix Skaarup <niki@skaarup.dev>
2023-06-08 22:44:20 +0200

    Update recheck.ts

--- src/lib/server/services/qbt/torrents/recheck.ts
+++ src/lib/server/services/qbt/torrents/recheck.ts
@@ -1,10 +1,37 @@
+import shared from '../shared';
+
 export default {
-	single: async (hashes: string[]): Promise<void> => {
+	single: async (hashes: string[]) => {
+		const input = `${shared.baseUrl}/torrents/recheck`;
 		const hashesToRecheck = hashes.join('|');
-		throw new Error('Not implemented');
+		const response = await fetch(`${input}?hashes=${hashesToRecheck}`, {
+			method: 'GET',
+			headers: {
+				accept: 'application/json'
+			}
+		});
+
+		if (!response.ok) {
+			throw new Error(response.statusText);
+		}
+
+		const data = await response.text();
+		return data;
 	},
-	all: async (): Promise<void> => {
-		// parameter "hashes" set to all
-		throw new Error('Not implemented');
+	all: async () => {
+		const input = `${shared.baseUrl}/torrents/recheck`;
+		const response = await fetch(`${input}?hashes=all`, {
+			method: 'GET',
+			headers: {
+				accept: 'application/json'
+			}
+		});
+
+		if (!response.ok) {
+			throw new Error(response.statusText);
+		}
+
+		const data = await response.text();
+		return data;
 	}
 };
